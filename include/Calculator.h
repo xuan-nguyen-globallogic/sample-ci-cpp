d038d6d9df777f80ee513f07a76ff858f64f67fe
xuan-nguyen-globallogic <xuan.nguyen@vizio.com>
2024-12-20 14:53:07 -0600

    resubmit format

--- include/Calculator.h
+++ include/Calculator.h
@@ -2,16 +2,16 @@
 #define CALCULATOR_H
 
 class Calculator {
- private:
-  int m_lastResult;
+   private:
+    int m_lastResult;
 
- public:
-  Calculator();
-  ~Calculator();
-  bool add(int addend1, int addend2);
-  bool mul(int factor1, int factor2);
-  bool div(int dividend, int divisor);
-  int get_last_result();
+   public:
+    Calculator();
+    ~Calculator();
+    bool add(int addend1, int addend2);
+    bool mul(int factor1, int factor2);
+    bool div(int dividend, int divisor);
+    int get_last_result();
 };
 
 #endif  // CALCULATOR_H
