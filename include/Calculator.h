007b21a3d0ac738184d3ded58b6deaf98c3ed3ec
xuan-nguyen-globallogic <xuan.nguyen@vizio.com>
2024-12-20 14:49:19 -0600

    changing format

--- include/Calculator.h
+++ include/Calculator.h
@@ -1,18 +1,17 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-class Calculator
-{
-private:
-    int m_lastResult;
+class Calculator {
+ private:
+  int m_lastResult;
 
-public:
-    Calculator();
-    ~Calculator();
-    bool add(int addend1, int addend2);
-    bool mul(int factor1, int factor2);
-    bool div(int dividend, int divisor);
-    int get_last_result();
+ public:
+  Calculator();
+  ~Calculator();
+  bool add(int addend1, int addend2);
+  bool mul(int factor1, int factor2);
+  bool div(int dividend, int divisor);
+  int get_last_result();
 };
 
-#endif // CALCULATOR_H
+#endif  // CALCULATOR_H
--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -1,41 +1,28 @@
 #include "Calculator.h"
 
-Calculator::Calculator()
-{
-    m_lastResult = 0;
-}
+Calculator::Calculator() { m_lastResult = 0; }
 
-Calculator::~Calculator()
-{
+Calculator::~Calculator() {}
 
-}
-
-bool Calculator::add(int addend1, int addend2)
-{
-    m_lastResult = addend1 + addend2;
-    return true;
+bool Calculator::add(int addend1, int addend2) {
+  m_lastResult = addend1 + addend2;
+  return true;
 }
 
 // This is for checking that code coverage will not be <100%
-bool Calculator::mul(int factor1, int factor2)
-{
-    m_lastResult = factor1 * factor2;
-    return true;
+bool Calculator::mul(int factor1, int factor2) {
+  m_lastResult = factor1 * factor2;
+  return true;
 }
 
-bool Calculator::div(int dividend, int divisor)
-{
-    if (divisor == 0)
-    {
-        m_lastResult = 0;
-        return false;
-    }
+bool Calculator::div(int dividend, int divisor) {
+  if (divisor == 0) {
+    m_lastResult = 0;
+    return false;
+  }
 
-    m_lastResult = dividend / divisor;
-    return true;
+  m_lastResult = dividend / divisor;
+  return true;
 }
 
-int Calculator::get_last_result()
-{
-    return m_lastResult;
-}
+int Calculator::get_last_result() { return m_lastResult; }
